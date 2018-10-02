8d8db16230d551f4a49a403b8fb921b9dec8ffb6
zach rice <zricer@protonmail.com>
2018-10-01 21:14:58 -0500

    bumping version

--- main.go
+++ main.go
@@ -119,7 +119,7 @@ type Config struct {
 }
 
 const defaultGithubURL = "https://api.github.com/"
-const version = "1.5.0"
+const version = "1.6.0"
 const defaultConfig = `
 title = "gitleaks config"
 # add regexes to the regex table
