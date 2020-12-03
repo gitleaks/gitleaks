b8accd72738343008d469103874ba1ea6951ba6d
zricethezav <zricer@protonmail.com>
2020-12-03 15:43:06 -0500

    rm unimplemented cleanpath option

--- options/options.go
+++ options/options.go
@@ -26,7 +26,6 @@ type Options struct {
 	ConfigPath     string `short:"c" long:"config-path" description:"Path to config"`
 	RepoConfigPath string `long:"repo-config-path" description:"Path to gitleaks config relative to repo root"`
 	ClonePath      string `long:"clone-path" description:"Path to clone repo to disk"`
-	CleanUp        string `long:"clone-cleanup" description:"Deletes cloned repo after scan"`
 	Version        bool   `long:"version" description:"Version number"`
 	Username       string `long:"username" description:"Username for git repo"`
 	Password       string `long:"password" description:"Password for git repo"`
