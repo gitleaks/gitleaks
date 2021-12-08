6a4c32d6d46208d53c9bcd09413ac2caa44cf6da
Zach Rice <zricer@protonmail.com>
2021-12-07 19:05:46 -0600

    remove indexing, slow is better than wrong

--- detect/detect.go
+++ detect/detect.go
@@ -121,10 +121,10 @@ func printFinding(f report.Finding) {
 
 func dedupe(findings []report.Finding) []report.Finding {
 	var retFindings []report.Finding
-	for i, f := range findings {
+	for _, f := range findings {
 		include := true
 		if strings.Contains(strings.ToLower(f.RuleID), "generic") {
-			for _, fPrime := range findings[i-i:] {
+			for _, fPrime := range findings {
 				if f.StartLine == fPrime.StartLine &&
 					f.EndLine == fPrime.EndLine &&
 					f.Commit == fPrime.Commit &&
