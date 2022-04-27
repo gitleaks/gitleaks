c5fe8294d9469ce5aa0681f9a7fbaea376577cb2
Zachary <zricezrice@gmail.com>
2022-04-27 07:25:54 -0500

    Removing private keyword from private key rule

--- cmd/generate/config/rules/privatekey.go
+++ cmd/generate/config/rules/privatekey.go
@@ -12,12 +12,17 @@ func PrivateKey() *config.Rule {
 		Description: "Private Key",
 		RuleID:      "private-key",
 		Regex:       regexp.MustCompile(`(?i)-----BEGIN[ A-Z0-9_-]{0,100}PRIVATE KEY-----[\s\S-]*KEY----`),
-		Keywords:    []string{"-----BEGIN PRIVATE"},
+		Keywords:    []string{"-----BEGIN"},
 	}
 
 	// validate
 	tps := []string{`-----BEGIN PRIVATE KEY-----
 anything
------END PRIVATE KEY-----`} // gitleaks:allow
+-----END PRIVATE KEY-----`,
+		`-----BEGIN RSA PRIVATE KEY-----
+abcdefghijklmnopqrstuvwxyz
+-----END RSA PRIVATE KEY-----
+`,
+	} // gitleaks:allow
 	return validate(r, tps, nil)
 }
