f2edea753c76849f6e625d42a5ac91941e8d9c5f
Zachary <zricezrice@gmail.com>
2022-06-20 14:06:16 -0500

    maintain parity with recent changes... need to create rule contributing guidelines

--- cmd/generate/config/main.go
+++ cmd/generate/config/main.go
@@ -112,7 +112,7 @@ func main() {
 		log.Fatal().Err(err).Msg("Failed to parse template")
 	}
 
-	f, err := os.Create("gitleaks.toml")
+	f, err := os.Create("../../../config/gitleaks.toml")
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to create rules.toml")
 	}
