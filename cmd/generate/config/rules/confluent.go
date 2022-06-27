4424d133204e12a73f366bffb643e75513f9ce48
Zachary <zricezrice@gmail.com>
2022-06-27 08:54:23 -0500

    remove ssns from allowlist since default config does not detect ssn

--- cmd/generate/config/rules/confluent.go
+++ cmd/generate/config/rules/confluent.go
@@ -10,11 +10,7 @@ func ConfluentSecretKey() *config.Rule {
 	r := config.Rule{
 		RuleID:      "confluent-secret-key",
 		Description: "Confluent Secret Key",
-<<<<<<< HEAD
-		Regex:       generateSemiGenericRegex([]string{"confluent"}, `[a-zA-Z-0-9]{64}`),
-=======
 		Regex:       generateSemiGenericRegex([]string{"confluent"}, alphaNumeric("64")),
->>>>>>> 73a3cf8afbfba27e46a9efbbe29c7f262569d721
 		SecretGroup: 1,
 		Keywords: []string{
 			"confluent",
@@ -23,11 +19,7 @@ func ConfluentSecretKey() *config.Rule {
 
 	// validate
 	tps := []string{
-<<<<<<< HEAD
-		generateSampleSecret("confluent", secrets.NewSecret(`[a-zA-Z-0-9]{64}`)),
-=======
 		generateSampleSecret("confluent", secrets.NewSecret(alphaNumeric("64"))),
->>>>>>> 73a3cf8afbfba27e46a9efbbe29c7f262569d721
 	}
 	return validate(r, tps, nil)
 }
@@ -37,11 +29,7 @@ func ConfluentAccessToken() *config.Rule {
 	r := config.Rule{
 		RuleID:      "confluent-access-token",
 		Description: "Confluent Access Token",
-<<<<<<< HEAD
-		Regex:       generateSemiGenericRegex([]string{"confluent"}, `[a-zA-Z-0-9]{16}`),
-=======
 		Regex:       generateSemiGenericRegex([]string{"confluent"}, alphaNumeric("16")),
->>>>>>> 73a3cf8afbfba27e46a9efbbe29c7f262569d721
 		SecretGroup: 1,
 		Keywords: []string{
 			"confluent",
@@ -50,11 +38,7 @@ func ConfluentAccessToken() *config.Rule {
 
 	// validate
 	tps := []string{
-<<<<<<< HEAD
-		generateSampleSecret("confluent", secrets.NewSecret(`[a-zA-Z-0-9]{16}`)),
-=======
 		generateSampleSecret("confluent", secrets.NewSecret(alphaNumeric("16"))),
->>>>>>> 73a3cf8afbfba27e46a9efbbe29c7f262569d721
 	}
 	return validate(r, tps, nil)
 }
