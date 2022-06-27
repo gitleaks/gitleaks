2f4eef38aed55dec8075a2e62f9eac6a52c7137d
Zachary <zricezrice@gmail.com>
2022-06-27 18:48:23 -0500

    Fix plaid, add plaid-access-token

--- cmd/generate/config/rules/plaid.go
+++ cmd/generate/config/rules/plaid.go
@@ -1,6 +1,8 @@
 package rules
 
 import (
+	"fmt"
+
 	"github.com/zricethezav/gitleaks/v8/cmd/generate/secrets"
 	"github.com/zricethezav/gitleaks/v8/config"
 )
@@ -24,11 +26,11 @@ func PlaidAccessID() *config.Rule {
 	return validate(r, tps, nil)
 }
 
-func PlaidAccessToken() *config.Rule {
+func PlaidSecretKey() *config.Rule {
 	// define rule
 	r := config.Rule{
-		RuleID:      "plaid-api-secret",
-		Description: "Plaid API Secret",
+		RuleID:      "plaid-secret-key",
+		Description: "Plaid Secret key",
 		Regex:       generateSemiGenericRegex([]string{"plaid"}, alphaNumeric("30")),
 		SecretGroup: 1,
 		Keywords: []string{
@@ -39,6 +41,26 @@ func PlaidAccessToken() *config.Rule {
 	// validate
 	tps := []string{
 		generateSampleSecret("plaid", secrets.NewSecret(alphaNumeric("30"))),
+	}
+	return validate(r, tps, nil)
+}
+
+func PlaidAccessToken() *config.Rule {
+	// define rule
+	r := config.Rule{
+		RuleID:      "plaid-api-token",
+		Description: "Plaid API Token",
+		Regex: generateSemiGenericRegex([]string{"plaid"},
+			fmt.Sprintf("access-(?:sandbox|development|production)-%s", hex8_4_4_4_12())),
+		SecretGroup: 1,
+		Keywords: []string{
+			"plaid",
+		},
+	}
+
+	// validate
+	tps := []string{
+		generateSampleSecret("plaid", secrets.NewSecret(fmt.Sprintf("access-(?:sandbox|development|production)-%s", hex8_4_4_4_12()))),
 	}
 	return validate(r, tps, nil)
 }
