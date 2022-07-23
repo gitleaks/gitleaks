a4acccc3b6791c68a11cdd2e359eac915ae67138
Zachary <zricezrice@gmail.com>
2022-07-22 22:20:42 -0500

    working on default and path config extensions

--- config/config.go
+++ config/config.go
@@ -5,10 +5,16 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 )
 
 //go:embed gitleaks.toml
 var DefaultConfig string
+var extendDepth int
+
+const maxExtendDepth = 2
 
 // ViperConfig is the config struct used by the Viper config package
 // to parse the config file. This struct does not include regular expressions.
@@ -46,7 +52,7 @@ type Config struct {
 	Extends     Extends
 	Path        string
 	Description string
-	Rules       []*Rule
+	Rules       map[string]Rule
 	Allowlist   Allowlist
 	Keywords    []string
 }
@@ -60,10 +66,9 @@ type Extends struct {
 }
 
 func (vc *ViperConfig) Translate() (Config, error) {
-	var (
-		rules    []*Rule
-		keywords []string
-	)
+	var keywords []string
+	rulesMap := make(map[string]Rule)
+
 	for _, r := range vc.Rules {
 		var allowlistRegexes []*regexp.Regexp
 		for _, a := range r.Allowlist.Regexes {
@@ -98,7 +103,7 @@ func (vc *ViperConfig) Translate() (Config, error) {
 		} else {
 			configPathRegex = regexp.MustCompile(r.Path)
 		}
-		r := &Rule{
+		r := Rule{
 			Description: r.Description,
 			RuleID:      r.ID,
 			Regex:       configRegex,
@@ -117,7 +122,8 @@ func (vc *ViperConfig) Translate() (Config, error) {
 		if r.Regex != nil && r.SecretGroup > r.Regex.NumSubexp() {
 			return Config{}, fmt.Errorf("%s invalid regex secret group %d, max regex secret group %d", r.Description, r.SecretGroup, r.Regex.NumSubexp())
 		}
-		rules = append(rules, r)
+		rulesMap[r.RuleID] = r
+		// rules = append(rules, r)
 	}
 	var allowlistRegexes []*regexp.Regexp
 	for _, a := range vc.Allowlist.Regexes {
@@ -130,7 +136,7 @@ func (vc *ViperConfig) Translate() (Config, error) {
 	c := Config{
 		Description: vc.Description,
 		Extends:     vc.Extends,
-		Rules:       rules,
+		Rules:       rulesMap,
 		Allowlist: Allowlist{
 			Regexes:   allowlistRegexes,
 			Paths:     allowlistPaths,
@@ -140,7 +146,13 @@ func (vc *ViperConfig) Translate() (Config, error) {
 		Keywords: keywords,
 	}
 
-	if c.Extends.UseDefault {
+	if maxExtendDepth != extendDepth {
+		// if the user supplied
+		if c.Extends.UseDefault {
+			c.extendDefault()
+		} else if c.Extends.Path != "" {
+			c.extendPath()
+		}
 
 	}
 
@@ -148,11 +160,65 @@ func (vc *ViperConfig) Translate() (Config, error) {
 }
 
 func (c *Config) extendDefault() {
+    extendDepth++
+	viper.SetConfigType("toml")
+	if err := viper.ReadConfig(strings.NewReader(DefaultConfig)); err != nil {
+		log.Fatal().Msgf("err reading toml %s", err.Error())
+	}
+	defaultViperConfig := ViperConfig{}
+	if err := viper.Unmarshal(&defaultViperConfig); err != nil {
+		log.Fatal().Err(err).Msg("Failed to load config")
+	}
+	cfg, err := defaultViperConfig.Translate()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to load config")
+	}
+
+	// iterate through default rules and add whichever rules
+	// are not in the base config rules but are in the default rules
+	for defaultRuleID, defaultRule := range cfg.Rules {
+		if _, ok := c.Rules[defaultRuleID]; !ok {
+			c.Rules[defaultRuleID] = defaultRule
+			c.Keywords = append(c.Keywords, defaultRule.Keywords...)
+		}
+	}
+
+	// append allowlists, not attempting to merge
+	c.Allowlist.Commits = append(c.Allowlist.Commits, cfg.Allowlist.Commits...)
+	c.Allowlist.Paths = append(c.Allowlist.Paths, cfg.Allowlist.Paths...)
+	c.Allowlist.Regexes = append(c.Allowlist.Regexes, cfg.Allowlist.Regexes...)
 
 }
 
 func (c *Config) extendPath() {
+    extendDepth++
+    fmt.Println("extending")
+	viper.SetConfigFile(c.Extends.Path)
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal().Msgf("Unable to load gitleaks config, err: %s", err)
+	}
+	defaultViperConfig := ViperConfig{}
+	if err := viper.Unmarshal(&defaultViperConfig); err != nil {
+		log.Fatal().Err(err).Msg("Failed to load config")
+	}
+	cfg, err := defaultViperConfig.Translate()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to load config")
+	}
 
+	// iterate through default rules and add whichever rules
+	// are not in the base config rules but are in the default rules
+	for defaultRuleID, defaultRule := range cfg.Rules {
+		if _, ok := c.Rules[defaultRuleID]; !ok {
+			c.Rules[defaultRuleID] = defaultRule
+			c.Keywords = append(c.Keywords, defaultRule.Keywords...)
+		}
+	}
+
+	// append allowlists, not attempting to merge
+	c.Allowlist.Commits = append(c.Allowlist.Commits, cfg.Allowlist.Commits...)
+	c.Allowlist.Paths = append(c.Allowlist.Paths, cfg.Allowlist.Paths...)
+	c.Allowlist.Regexes = append(c.Allowlist.Regexes, cfg.Allowlist.Regexes...)
 }
 
 func (c *Config) extendURL() {
