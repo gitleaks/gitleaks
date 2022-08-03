58b341b614fb1ef7f5e7fa5aba88d749f327d30e
maltemorgenstern <65773564+maltemorgenstern@users.noreply.github.com>
2022-08-03 17:34:35 +0200

    Configure max length for vault rules

--- cmd/generate/config/rules/vault.go
+++ cmd/generate/config/rules/vault.go
@@ -10,7 +10,7 @@ func VaultServiceToken() *config.Rule {
 	r := config.Rule{
 		Description: "Vault Service Token",
 		RuleID:      "vault-service-token",
-		Regex:       generateUniqueTokenRegex(`hvs\.[a-z0-9_-]{90,}`),
+		Regex:       generateUniqueTokenRegex(`hvs\.[a-z0-9_-]{90,100}`),
 		Keywords:    []string{"hvs"},
 	}
 
@@ -26,7 +26,7 @@ func VaultBatchToken() *config.Rule {
 	r := config.Rule{
 		Description: "Vault Batch Token",
 		RuleID:      "vault-batch-token",
-		Regex:       generateUniqueTokenRegex(`hvb\.[a-z0-9_-]{138,}`),
+		Regex:       generateUniqueTokenRegex(`hvb\.[a-z0-9_-]{138,212}`),
 		Keywords:    []string{"hvb"},
 	}
 
