190ac977be1256cc29da10b9d62c18a6dce927b3
Oleksandr Redko <Oleksandr_Redko@epam.com>
2023-06-05 18:43:04 +0300

    Fix closing file in writeJson and writeSarif (#1187)

--- report/json.go
+++ report/json.go
@@ -9,6 +9,8 @@ func writeJson(findings []Finding, w io.WriteCloser) error {
 	if len(findings) == 0 {
 		findings = []Finding{}
 	}
+	defer w.Close()
+
 	encoder := json.NewEncoder(w)
 	encoder.SetIndent("", " ")
 	return encoder.Encode(findings)
