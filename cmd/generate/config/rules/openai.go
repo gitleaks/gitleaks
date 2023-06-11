8d66d8e53e2e2a84ac48abefc45f6fa72a46291a
mkthebea <5596molly@naver.com>
2023-06-11 17:12:09 +0900

    Add more specific patterns for openAI token detection

--- cmd/generate/config/rules/openai.go
+++ cmd/generate/config/rules/openai.go
@@ -1,9 +1,9 @@
 package rules
 
 import (
-  "regexp"
-  
-  "github.com/zricethezav/gitleaks/v8/config"
+	"regexp"
+
+	"github.com/zricethezav/gitleaks/v8/config"
 )
 
 func OpenAI() *config.Rule {
@@ -11,13 +11,14 @@ func OpenAI() *config.Rule {
     r := config.Rule{
         Description: "Open AI token",
         RuleID:      "open-ai-token",
-        Regex: regexp.MustCompile("(sk-)[a-zA-Z0-9]{48}"),
+        Regex: regexp.MustCompile("(sk-)[a-zA-Z0-9]{20}(T3BlbkFJ)[a-zA-Z0-9]{20}"),
+        SecretGroup: 1,
         Keywords: []string{"sk-",},
     }
 
     // validate
     tps := []string{
-        generateSampleSecret("OpenAI", "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"),
+        generateSampleSecret("OpenAI", "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"),
     }
     return validate(r, tps, nil)
 }
