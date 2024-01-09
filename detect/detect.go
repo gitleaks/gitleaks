8b10d85edbdff219029120eba1c3ec644038fe88
Kemosabert <bert.coppens14@gmail.com>
2024-01-09 11:08:01 +0100

    report the whole line on which the finding was found

--- detect/detect.go
+++ detect/detect.go
@@ -283,7 +283,7 @@ func (d *Detector) detectRule(fragment Fragment, rule config.Rule) []report.Find
 			Secret:      secret,
 			Match:       secret,
 			Tags:        rule.Tags,
-			Line:        fragment.Raw[loc.startLineIndex:loc.endLineIndex],
+			Line:        fragment.Raw[0:],
 		}
 
 		if strings.Contains(fragment.Raw[loc.startLineIndex:loc.endLineIndex],
