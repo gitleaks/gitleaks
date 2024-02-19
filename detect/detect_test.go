46597c87e59ea4f3d3dd65e00568d8dc84d85014
Baruch Odem <baruchiro@gmail.com>
2024-02-19 16:05:44 +0200

    Add SecretGroup to AlibabaAccessKey rule

--- detect/detect_test.go
+++ detect/detect_test.go
@@ -339,13 +339,13 @@ func TestDetect(t *testing.T) {
 			expectedFindings: []report.Finding{
 				{
 					Description: "Detected an Alibaba Cloud AccessKey ID, posing a risk of unauthorized cloud resource access and potential data compromise.",
-					Match:       `LTAIe7322523fb86ed64c836`,
+					Match:       `LTAIe7322523fb86ed64c836"`,
 					Secret:      `LTAIe7322523fb86ed64c836`,
 					Line:        `alibabaKey := "LTAIe7322523fb86ed64c836"`,
 					File:        "tmp.py",
 					RuleID:      "alibaba-access-key-id",
 					Tags:        []string{},
-					Entropy:     3.9132698,
+					Entropy:     3.8239348,
 					StartLine:   0,
 					EndLine:     0,
 					StartColumn: 16,
