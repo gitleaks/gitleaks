9ba5bb80067a054a9b02f5ab6c90d7ea0f1489c8
Baruch Odem <baruchiro@gmail.com>
2024-02-22 16:00:52 +0200

    AWS access key is base32 encoded string
    Fix #1324 Change AWS regex to match only allowed characters

--- cmd/generate/config/rules/aws.go
+++ cmd/generate/config/rules/aws.go
@@ -2,13 +2,12 @@ package rules
 
 import (
 	"regexp"
-	"strings"
 
 	"github.com/zricethezav/gitleaks/v8/cmd/generate/secrets"
 	"github.com/zricethezav/gitleaks/v8/config"
 )
 
-var credFileAccessKey = "aws_access_key_id=ASIA" + strings.ToUpper(secrets.NewSecret(hex("16")))
+var credFileAccessKey = "aws_access_key_id=AKIALALEMEL33243OLIB" // gitleaks:allow
 var credFileSecretKey = "aws_secret_access_key=" + secrets.NewSecret(hex("40"))
 var credFileSessionToken = "aws_session_token=" + secrets.NewSecret(hex("928"))
 
@@ -18,7 +17,7 @@ func AWS() *config.Rule {
 		Description: "Identified a pattern that may indicate AWS credentials, risking unauthorized cloud resource access and data breaches on AWS platforms.",
 		RuleID:      "aws-access-token",
 		Regex: regexp.MustCompile(
-			"(?:A3T[A-Z0-9]|AKIA|ASIA|ABIA|ACCA)[A-Z0-9]{16}"),
+			"(?:A3T[A-Z0-9]|AKIA|ASIA|ABIA|ACCA)[A-Z2-7]{16}"),
 		Keywords: []string{
 			"AKIA",
 			"ASIA",
@@ -33,6 +32,7 @@ func AWS() *config.Rule {
 		credFileAccessKey,
 	}
 	fps := []string{
+		generateSampleSecret("AWS", "AKIALALEMEL33243O000"), // includes 0 which can't be result of base32 encoding
 		`"RoleId": "AROAWORVRXQ5NC76T7223"`,
 		credFileSecretKey,
 		credFileSessionToken,
