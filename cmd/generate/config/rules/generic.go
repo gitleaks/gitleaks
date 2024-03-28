93df1a495886e2b1ebf70923e95e1d7e05038217
Baruch Odem (Rothkoff) <baruchiro@gmail.com>
2024-03-28 16:13:04 +0200

    feat: catch secret in XML child (#1)
    ### Description:
    - Added generic API key inside an XML element:
    `<password>edf8f16608465858a6c9e3cccb97d3c2</password>`
    - Added True-Positives and False-Positives to the Generic API Key rule
    
    ### Checklist:
    
    * [x] Does your PR pass tests?
    * [x] Have you written new tests for your changes?
    * [x] Have you lint your code locally prior to submission?
    
    Original: https://github.com/gitleaks/gitleaks/pull/1265

--- cmd/generate/config/rules/generic.go
+++ cmd/generate/config/rules/generic.go
@@ -43,12 +43,26 @@ func GenericCredential() *config.Rule {
 		generateSampleSecret("generic", "Zf3D0LXCM3EIMbgJpUNnkRtOfOueHznB"),
 		`"client_id" : "0afae57f3ccfd9d7f5767067bc48b30f719e271ba470488056e37ab35d4b6506"`,
 		`"client_secret" : "6da89121079f83b2eb6acccf8219ea982c3d79bccc3e9c6a85856480661f8fde",`,
+		`"password: 'edf8f16608465858a6c9e3cccb97d3c2'"`,
+		"<password>edf8f16608465858a6c9e3cccb97d3c2</password>",
+		`<element password="edf8f16608465858a6c9e3cccb97d3c2" />`,
+		"M_DB_PASSWORD= edf8f16608465858a6c9e3cccb97d3c2",
 	}
 	fps := []string{
 		`client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id`,
 		`password combination.
 
-R5: Regulatory--21`,
+		R5: Regulatory--21`,
+
+		`"client_id" : "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"`,
+		`"client_secret" : "4v7b9n2k5h",`, // entropy: 3.32
+		`"password: 'comp123!'"`,
+		"<password>MyComp9876</password>", // entropy: 3.32
+		`<element password="Comp4567@@" />`,
+		"M_DB_PASSWORD= aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+		"GITHUB_TOKEN: ${GITHUB_TOKEN}",
+		"password = 'your_password_here'",
+		"https://google.com?user=abc&password=123",
 	}
 	return validate(r, tps, fps)
 }
