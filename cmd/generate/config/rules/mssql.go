ae7a90d674b2b5eb9cc514d94a07b7accf716882
fhverga <69872236+fhverga@users.noreply.github.com>
2024-04-18 16:37:57 -0300

    Update mssql.go

--- cmd/generate/config/rules/mssql.go
+++ cmd/generate/config/rules/mssql.go
@@ -1,33 +1,33 @@
 package rules
 
 import (
-    "regexp"
+	"regexp"
 
-    "github.com/zricethezav/gitleaks/v8/config"
+	"github.com/zricethezav/gitleaks/v8/config"
 )
 
 // MSSQLDatabaseCredentials generates a rule for detecting exposure of MSSQL database credentials.
 func MSSQLDatabaseCredentials() *config.Rule {
-    // Define Rule
-    r := config.Rule{
-        // Human readable description of the rule
-        Description: "Detects exposure of MSSQL database credentials",
+	// Define Rule
+	r := config.Rule{
+		// Human readable description of the rule
+		Description: "Detects exposure of MSSQL database credentials",
 
-        // Unique ID for the rule
-        RuleID:      "mssql-database-credentials",
+		// Unique ID for the rule
+		RuleID: "mssql-database-credentials",
 
-        // Regex used for detecting secrets
-        Regex: regexp.MustCompile(
-            `Password=[^;]+`),
+		// Regex used for detecting secrets
+		Regex: regexp.MustCompile(
+			`Password=[^;]+`),
 
-        // Keywords used for string matching on fragments (pre-filter)
-        Keywords:    []string{"MSSQL", "credentials"},
-    }
+		// Keywords used for string matching on fragments (pre-filter)
+		Keywords: []string{"MSSQL", "credentials"},
+	}
 
-    // Validate rule
-    tps := []string{
-        // Example secrets that match the rule
-        "Password=mySecurePassword123;",
-    }
-    return validate(r, tps, nil)
+	// Validate rule
+	tps := []string{
+		// Example secrets that match the rule
+		"Password=mySecurePassword123;",
+	}
+	return validate(r, tps, nil)
 }
