5fdc43c2dd76e804a20ab44143f3b97e1e1ddf3b
Stijn Taelemans <stijn@digita.ai>
2024-07-04 17:16:23 +0200

    feat: update hubspot rules

--- cmd/generate/config/rules/hubspot.go
+++ cmd/generate/config/rules/hubspot.go
@@ -4,13 +4,16 @@ import (
 	"github.com/zricethezav/gitleaks/v8/config"
 )
 
-func HubSpot() *config.Rule {
+func HubSpotAPIKey() *config.Rule {
 	// define rule
 	r := config.Rule{
 		Description: "Found a HubSpot API Token, posing a risk to CRM data integrity and unauthorized marketing operations.",
 		RuleID:      "hubspot-api-key",
-		Regex: generateSemiGenericRegex([]string{"hubspot"},
-			`[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}`, true),
+		Regex: generateSemiGenericRegex(
+			[]string{"hubspot"},
+			`[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}`,
+			true,
+		),
 
 		Keywords: []string{"hubspot"},
 	}
@@ -18,6 +21,48 @@ func HubSpot() *config.Rule {
 	// validate
 	tps := []string{
 		`const hubspotKey = "12345678-ABCD-ABCD-ABCD-1234567890AB"`, // gitleaks:allow
+	}
+	return validate(r, tps, nil)
+}
+
+func HubSpotPrivateAppAccessToken() *config.Rule {
+	// define rule
+	r := config.Rule{
+		Description: "Found a HubSpot Private App API Token, posing a risk to CRM data integrity and unauthorized marketing operations.",
+		RuleID:      "hubspot-api-key",
+		Regex: generateSemiGenericRegex(
+			[]string{"hubspot"},
+			`pat-(?:eu|na)/d-[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}`,
+			true,
+		),
+
+		Keywords: []string{"hubspot"},
+	}
+
+	// validate
+	tps := []string{
+		`const hubspotKey = "pat-eu1-12345678-ABCD-ABCD-ABCD-1234567890AB"`, // gitleaks:allow
+	}
+	return validate(r, tps, nil)
+}
+
+func HubSpotDeveloperAPIKey() *config.Rule {
+	// define rule
+	r := config.Rule{
+		Description: "Found a HubSpot Private App API Token, posing a risk to CRM data integrity and unauthorized marketing operations.",
+		RuleID:      "hubspot-api-key",
+		Regex: generateSemiGenericRegex(
+			[]string{"hubspot"},
+			`(?:eu|na)/d-[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12)`,
+			true,
+		),
+
+		Keywords: []string{"hubspot"},
+	}
+
+	// validate
+	tps := []string{
+		`const hubspotKey = "eu1-1234-ABCD-1234-ABCD-1234567890AB"`, // gitleaks:allow
 	}
 	return validate(r, tps, nil)
 }
