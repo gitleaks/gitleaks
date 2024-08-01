a9c83aa2018172066c71a11f2f20baaa0965f536
localleon <me@lrau.xyz>
2024-08-01 09:31:29 +0200

    (feat): Implementing proxmitiy matching for data-key in kubernetes secrets
    Signed-off-by: localleon <me@lrau.xyz>

--- cmd/generate/config/rules/kubernetes.go
+++ cmd/generate/config/rules/kubernetes.go
@@ -6,13 +6,16 @@ import (
 	"github.com/zricethezav/gitleaks/v8/config"
 )
 
-func KubernetesSecret() *config.Rule {
+// The kubernetes rules are split into two functions to make the complex proximity matching of the data-key and the kind-identifier more readable and testable
+
+// KubernetesSecretWithDataBefore validates if we detected a kubernetes secret which contains data, before the resource identifier!
+func KubernetesSecretWithDataBefore() *config.Rule {
 	// define rule
 	r := config.Rule{
-		RuleID:      "kubernetes-secret",
+		RuleID:      "kubernetes-secret-with-data-before",
 		Description: "Possible Kubernetes Secret detected, posing a risk of leaking credentials/tokens from your deployments",
-		// We only match based on the "kind:secret" value of all kubernetes secrets
-		Regex: generateUniqueTokenRegex(`kind:\s*Secret`, true),
+		// We try to match secrets by looking if we have the keyword
+		Regex: generateUniqueTokenRegex(`(?i)(?:\b(data)\b)\W+(?:\w+\W+){0,200}?\bkind:\s*Secret\b.*`, true),
 
 		Keywords: []string{
 			"Secret",
@@ -24,8 +27,36 @@ func KubernetesSecret() *config.Rule {
 	// validate
 	tps := map[string]string{
 		// Sample Kubernetes Secret from https://kubernetes.io/docs/concepts/configuration/secret/
-		"kubernetes.yaml": "apiVersion: v1 kind: Secret metadata: name: secret-sa-sample annotations: kubernetes.io/service-account.name: 'sa-name' type: kubernetes.io/service-account-token data: extra: YmFyCg==",                                                                      // gitleaks:allow
+		// These secrets contain the "data"-key before the actual identifier "kind: Secret"
+		"kubernetes.yaml": "apiVersion: v1 data: extra: YmFyCg== kind: Secret metadata: name: secret-sa-sample annotations: kubernetes.io/service-account.name: 'sa-name'",                                                                                                                // gitleaks:allow
 		"kubernetes.yml":  "apiVersion: v1 data: password: UyFCXCpkJHpEc2I9 username: YWRtaW4= kind: Secret metadata: creationTimestamp: '2022-06-28T17:44:13Z' name: db-user-pass namespace: default resourceVersion: '12708504' uid: 91becd59-78fa-4c85-823f-6d44436242ac type: Opaque", // gitleaks:allow
 	}
+	return validateWithPaths(r, tps, nil)
+}
+
+// KubernetesSecretWithDataAfter validates if we detected a kubernetes secret which contains data, after the resource identifier!
+func KubernetesSecretWithDataAfter() *config.Rule {
+	// define rule
+	r := config.Rule{
+		RuleID:      "kubernetes-secret-with-data-after",
+		Description: "Possible Kubernetes Secret detected, posing a risk of leaking credentials/tokens from your deployments",
+		// We try to match secrets by looking if we have the keyword
+		Regex: generateUniqueTokenRegex(`(?i)(?:\bkind:\s*Secret\b)\W+(?:\w+\W+){0,200}?\b(data)\b.*`, true),
+
+		Keywords: []string{
+			"Secret",
+		},
+		// Kubernetes secrets are always yaml files, we limit to common yaml-endings to make this rule more safe!
+		Path: regexp.MustCompile(`(?i)\.ya?ml$`),
+	}
+
+	// validate
+	tps := map[string]string{
+		// Sample Kubernetes Secret from https://kubernetes.io/docs/concepts/configuration/secret/
+		// These secrets contain the data after the  actual identifier "kind: Secret"
+		"kubernetes.yaml": "apiVersion: v1 kind: Secret data: extra: YmFyCg== metadata: name: secret-sa-sample annotations: kubernetes.io/service-account.name: 'sa-name'",                                                                                                                // gitleaks:allow
+		"kubernetes.yml":  "apiVersion: v1 kind: Secret data: password: UyFCXCpkJHpEc2I9 username: YWRtaW4= metadata: creationTimestamp: '2022-06-28T17:44:13Z' name: db-user-pass namespace: default resourceVersion: '12708504' uid: 91becd59-78fa-4c85-823f-6d44436242ac type: Opaque", // gitleaks:allow
+	}
+
 	return validateWithPaths(r, tps, nil)
 }
