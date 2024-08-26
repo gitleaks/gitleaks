cc840344a122ebe64dfa185817e83665245346a4
Richard Gomez <rmgomez368@gmail.com>
2024-08-26 16:10:06 -0400

    chore: fix gl workflow error

--- cmd/generate/config/rules/generic.go
+++ cmd/generate/config/rules/generic.go
@@ -40,7 +40,7 @@ func GenericCredential() *config.Rule {
 
 	// validate
 	tps := []string{
-		utils.GenerateSampleSecret("generic", "CLOJARS_34bf0e88955ff5a1c328d6a7491acc4f48e865a7b8dd4d70a70749037443"),
+		utils.GenerateSampleSecret("generic", "CLOJARS_34bf0e88955ff5a1c328d6a7491acc4f48e865a7b8dd4d70a70749037443"), //gitleaks:allow
 		utils.GenerateSampleSecret("generic", "Zf3D0LXCM3EIMbgJpUNnkRtOfOueHznB"),
 		`"client_id" : "0afae57f3ccfd9d7f5767067bc48b30f719e271ba470488056e37ab35d4b6506"`,
 		`"client_secret" : "6da89121079f83b2eb6acccf8219ea982c3d79bccc3e9c6a85856480661f8fde",`,
