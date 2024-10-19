18445ce5865f4bba33a214ed6b29c65ae77c5dfd
Richard Gomez <rmgomez368@gmail.com>
2024-10-19 12:54:00 -0400

    feat(config): ignore numerical sequences

--- cmd/generate/config/rules/hubspot.go
+++ cmd/generate/config/rules/hubspot.go
@@ -8,17 +8,20 @@ import (
 func HubSpot() *config.Rule {
 	// define rule
 	r := config.Rule{
-		Description: "Found a HubSpot API Token, posing a risk to CRM data integrity and unauthorized marketing operations.",
 		RuleID:      "hubspot-api-key",
+		Description: "Found a HubSpot API Token, posing a risk to CRM data integrity and unauthorized marketing operations.",
 		Regex: utils.GenerateSemiGenericRegex([]string{"hubspot"},
 			`[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}`, true),
-
+		Entropy:  3,
 		Keywords: []string{"hubspot"},
 	}
 
 	// validate
 	tps := []string{
+		`const hubspotKey = "b7bc6633-e1fc-431e-b812-4231e2ffdd2e"`, // gitleaks:allow
+	}
+	fps := []string{
 		`const hubspotKey = "12345678-ABCD-ABCD-ABCD-1234567890AB"`, // gitleaks:allow
 	}
-	return utils.Validate(r, tps, nil)
+	return utils.Validate(r, tps, fps)
 }
