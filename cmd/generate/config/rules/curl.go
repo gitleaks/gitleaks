e49f6539cc5ce954523fbf543065484badeaf5eb
Richard Gomez <rmgomez368@gmail.com>
2024-10-20 12:10:36 -0400

    fix(curl): update pattern & ignore more stuff

--- cmd/generate/config/rules/curl.go
+++ cmd/generate/config/rules/curl.go
@@ -13,15 +13,18 @@ func CurlBasicAuth() *config.Rule {
 	r := config.Rule{
 		RuleID: "curl-auth-user",
 		// TODO: Description: "",
-		Regex:    regexp.MustCompile(`\bcurl\b(?:.*|.*(?:[\r\n]{1,2}.*){1,5})[ \t\n\r](?:-u|--user)[ =](?:("[^:"]{3,}:[^"]{3,}")|('[^:']{3,}:[^']{3,}')|((?:"[^"]{3,}"|'[^']{3,}'|[\w$@.-]+):(?:"[^"]{3,}"|'[^']{3,}'|[\w$@.-]+))|)(?:\s|\z)`),
+		Regex:    regexp.MustCompile(`\bcurl\b(?:.*|.*(?:[\r\n]{1,2}.*){1,5})[ \t\n\r](?:-u|--user)(?:=|[ \t]{0,5})(?:"([^:"]{3,}:[^"]{3,})"|'([^:']{3,}:[^']{3,})'|((?:"[^"]{3,}"|'[^']{3,}'|[\w$@.-]+):(?:"[^"]{3,}"|'[^']{3,}'|[\w${}@.-]+)))(?:\s|\z)`),
 		Keywords: []string{"curl"},
+		Entropy:  2,
 		Allowlists: []config.Allowlist{
 			{
 				Regexes: []*regexp.Regexp{
-					regexp.MustCompile(`[^:]+:(changeme|pass(word)?|pwd|\*+|x+)`),    // common placeholder passwords
-					regexp.MustCompile(`<[^>]+>:<[^>]+>|<[^:]+:[^>]+>`),              // <placeholder>
-					regexp.MustCompile(`[^:]+:\[[^]]+]`),                             // [placeholder]
-					regexp.MustCompile(`(?i)[^:]+:\$(\d|[a-z]\w+|(\{\d|[a-z]\w+}))`), // $1 or $VARIABLE
+					regexp.MustCompile(`[^:]+:(change(it|me)|pass(word)?|pwd|test|token|\*+|x+)`), // common placeholder passwords
+					regexp.MustCompile(`<[^>]+>:<[^>]+>|<[^:]+:[^>]+>`),                           // <placeholder>
+					regexp.MustCompile(`[^:]+:\[[^]]+]`),                                          // [placeholder]
+					regexp.MustCompile(`[^:]+:\$(\d|\w+|\{(\d|\w+)})`),                            // $1 or $VARIABLE
+					regexp.MustCompile(`\$\([^)]+\):\$\([^)]+\)`),                                 // $(cat login.txt)
+					regexp.MustCompile(`\$?{{[^}]+}}:\$?{{[^}]+}}`),                               // ${{ secrets.FOO }} or {{ .Values.foo }}
 				},
 			},
 		},
@@ -37,6 +40,7 @@ func CurlBasicAuth() *config.Rule {
   -u developer:yqDVtkqPECriaLRi`, // different line
 
 		// long
+		`curl -sw '%{http_code}' -X POST --user  'johns:h0pk1ns~21s' $GItHUB_API_URL/$GIT_COMMIT --data`,
 		`curl --user roger23@gmail.com:pQ9wTxu4Fg https://www.dropbox.com/cli_link?host_id=abcdefg -v`, // same line, no quotes
 		`curl -s --user 'api:d2LkV78zLx!t' \
     https://api.mailgun.net/v2/sandbox91d3515882ecfaa1c65be642.mailgun.org/messages`, // same line, single quotes
@@ -52,13 +56,25 @@ func CurlBasicAuth() *config.Rule {
 	}
 	fps := []string{
 		// short
+		`curl -i -u 'test:test'`,
 		`   curl -sL --user "$1:$2" "$3" > "$4"`,                      // environment variable
 		`curl -u <user:password> https://test.com/endpoint`,           // placeholder
 		`curl --user neo4j:[PASSWORD] http://[IP]:7474/db/data/`,      // placeholder
 		`curl -u "myusername" http://localhost:15130/api/check_user/`, // no password
+		`curl -u username:token`,
+		`curl -u "${_username}:${_password}"`,
+		`curl -k -X POST -I -u "SRVC_JENKINS:${APPID}"`,
 
 		// long
-		`           curl -sL --user "$GITHUB_USERNAME:$GITHUB_PASSWORD" "$GITHUB_URL" > "$TESTS_PATH"`,                                                             // environment variable
+		`curl -sw '%{http_code}' -X POST --user '$USERNAME:$PASSWORD' $GItHUB_API_URL/$GIT_COMMIT --data`,
+		`curl --user "xxx:yyy"`,
+		`           curl -sL --user "$GITHUB_USERNAME:$GITHUB_PASSWORD" "$GITHUB_URL" > "$TESTS_PATH"`, // environment variable
+		// variable interpolation
+		`curl --silent --fail {{- if and $.Values.username $.Values.password }} --user "{{ $.Values.username }}:{{ $.Values.password }}"`,
+		`curl -XGET -i -u "${{ env.ELK_ID }}:${{ build.env.ELK_PASS }}"`,
+		`curl -XGET -i -u "${{needs.vault.outputs.account_id}}:${{needs.vault.outputs.account_password}}"`,
+		`curl -XGET -i -u "${{ steps.vault.outputs.account_id }}:${{ steps.vault.outputs.account_password }}"`,
+		`curl -X POST --user "$(cat ./login.txt):$(cat ./password.txt)"`,                                                                                           // command
 		`curl http://127.0.0.1:5000/file --user user:pass --digest        # digest auth`,                                                                           // placeholder
 		`   curl -X GET --insecure --user "username:password" \`,                                                                                                   // placeholder
 		`curl --silent --insecure --user ${f5user}:${f5pass} \`,                                                                                                    // placeholder
@@ -80,12 +96,13 @@ nc -u -l 41234`,
 // https://curl.se/docs/manpage.html#-H
 func CurlHeaderAuth() *config.Rule {
 	// language=regexp
-	authPat := `(?i)(?:Authorization:[ \t]?(?:Basic[ \t]([a-z0-9+/]{8,}={0,3})|(?:Bearer|Token)[ \t]([\w=@.+/-]{8,})|([\w=.+/-]{8,}))|(?:ApiKey|Token|X-API-KEY):[ \t]?([\w=@.+/-]{8,}))`
+	authPat := `(?i)(?:Authorization:[ \t]{0,5}(?:Basic[ \t]([a-z0-9+/]{8,}={0,3})|(?:Bearer|(?:Api-)?Token)[ \t]([\w=~@.+/-]{8,})|([\w=~@.+/-]{8,}))|(?:(?:X-)?Api-?(?:Key|Token)|Token):[ \t]{0,5}([\w=~@.+/-]{8,}))`
 	r := config.Rule{
 		RuleID: "curl-auth-header",
 		// TODO: Description: "",
 		Regex: regexp.MustCompile(
-			fmt.Sprintf(`\bcurl\b(?:.*?|.*?(?:[\r\n]{1,2}.*?){1,5})[ \t\n\r](?:-H|--header)[ =](?:"%s"|'%s')(?:\B|\s|\z)`, authPat, authPat)),
+			// language=regexp
+			fmt.Sprintf(`\bcurl\b(?:.*?|.*?(?:[\r\n]{1,2}.*?){1,5})[ \t\n\r](?:-H|--header)(?:=|[ \t]{0,5})(?:"%s"|'%s')(?:\B|\s|\z)`, authPat, authPat)),
 		Entropy:  2.75,
 		Keywords: []string{"curl"},
 		//Allowlists: []config.Allowlist{
@@ -96,6 +113,7 @@ func CurlHeaderAuth() *config.Rule {
 	}
 
 	tps := []string{
+		`curl --header  'Authorization:  5eb4223e-5008-46e5-be67-c7b8f2732305'`,
 		// Short flag.
 		`curl -H 'Authorization: Basic YnJvd3Nlcjo=' \`, // same line, single quotes
 		// TODO: Handle short flags combined.
@@ -105,7 +123,7 @@ func CurlHeaderAuth() *config.Rule {
 		`curl -k -X POST --header "Authorization: Basic djJlNEpYa0NJUHZ5a2FWT0VRXzRqZmZUdDkwYTp2emNBZGFzZWpmlWZiUDc2VUJjNDNNVDExclVh" "https://api-qa.example.com:8243/token" -d "grant_type=client_credentials"`, // same line, double quotes
 
 		// Basic auth.
-		` curl -X POST -H "Content-Type: application/json" \
+		`curl -X POST -H "Content-Type: application/json" \
  -H "Authorization: Basic MzUzYjMwMmM0NDU3NGY1NjUwNDU2ODdlNTM0ZTdkNmE6Mjg2OTI0Njk3ZTYxNWE2NzJhNjQ2YTQ5MzU0NTY0NmM=" \
   -d '{"user":{"emailAddress":"test@example.com"}, "password":"password"}' \
   'http://localhost:8080/oauth2-provider/v1.0/users'`, // different line, double quotes
@@ -126,18 +144,22 @@ func CurlHeaderAuth() *config.Rule {
 		`curl -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" \
      -H "Authorization: Bearer _FXNljbSRYMWx3TWrd7lgKhLtVZX6iskC8Wcbb4b" \
      -H "Content-Type:application/json"`,
+		`curl -H "Authorization: Bearer sha256~bRLFnzd59Z3XpZH5_seJPHALOuvbWiKwbFKSsoALkgp"`,
 
 		// Token auth
-		`curl -H "Authorization: Token 22cb987851bc5659229114c62e60c79abd0d2c08" --request PUT https://appsecclass.report/api/use/635`, // token
+		`curl -H "Authorization: Api-Token 22cb987851bc5659l29114c62e60c79abd0d2c08" --request PUT https://appsecclass.report/api/use/635`, // token
+		`curl -H "Authorization: Token 22cb987851bc5659229114c62e60c79abd0d2c08" --request PUT https://appsecclass.report/api/use/635`,     // token
 
 		// Nothing
 		`curl -L -H "Authorization:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb25maWRlbmNlIjowLjh9.kvRPfjAhLhtRTczoRgctVGp7KY1QVH3UBZM-gM0x8ec" service1.local -> correct jwt `, // no prefix
+		`curl -L -H "Authorization: sha256~bRLFnzd5@=-.a+/hgdS"`, // no prefix
 
 		// Non-authorization headers.
-		`curl -XPOST http://localhost:8080/api/tasks -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" -H "Token: 3fea6af1349166ea" -d "content=hello-curl"`, // token
 		`curl -X GET \
      -H "apikey: c4ed6c21-9dd5-4a05-8e3f-c56d1151cce8" \
-     -H "Accept: application/json" \`, // API Key placeholder
+     -H "Accept: application/json" \`, // apikey
+		`curl -X POST --header "Api-Token: Sk94HG7f6KB"`, // api-token
+		`curl -XPOST http://localhost:8080/api/tasks -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" -H "Token: 3fea6af1349166ea" -d "content=hello-curl"`, // token
 	}
 	fps := []string{
 		// Placeholders
