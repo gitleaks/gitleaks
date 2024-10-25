455621fa21d4c2850ae0ce412d7456178001bb9f
Oscar Blanco Castan <oscarbc1996@gmail.com>
2024-10-25 19:53:57 +0200

    add new jira api token pattern

--- cmd/generate/config/rules/atlassian.go
+++ cmd/generate/config/rules/atlassian.go
@@ -11,8 +11,18 @@ func Atlassian() *config.Rule {
 	r := config.Rule{
 		Description: "Detected an Atlassian API token, posing a threat to project management and collaboration tool security and data confidentiality.",
 		RuleID:      "atlassian-api-token",
-		Regex: utils.GenerateSemiGenericRegex([]string{
-			"atlassian", "confluence", "jira"}, utils.AlphaNumeric("24"), true),
+		Regex: utils.MergeRegexps(
+			utils.GenerateSemiGenericRegex(
+				[]string{"atlassian", "confluence", "jira"},
+				utils.AlphaNumeric("24"),
+				true,
+			),
+			utils.GenerateSemiGenericRegex(
+				[]string{"atlassian", "confluence", "jira"},
+				`[A-Za-z0-9_\-=]{192}`,
+				false,
+			),
+		),
 		Keywords: []string{"atlassian", "confluence", "jira"},
 	}
 
@@ -20,6 +30,7 @@ func Atlassian() *config.Rule {
 	tps := []string{
 		utils.GenerateSampleSecret("atlassian", secrets.NewSecret(utils.AlphaNumeric("24"))),
 		utils.GenerateSampleSecret("confluence", secrets.NewSecret(utils.AlphaNumeric("24"))),
+		utils.GenerateSampleSecret("jira", "ATATT3xFfGF0K3irG5tKKi-6u-wwaXQFeGwZ-IHR-hQ3CulkKtMSuteRQFfLZ6jihHThzZCg_UjnDt-4Wl_gIRf4zrZJs5JqaeuBhsfJ4W5GD6yGg3W7903gbvaxZPBjxIQQ7BgFDSkPS8oPispw4KLz56mdK-G6CIvLO6hHRrZHY0Q3tvJ6JxE=C63992E6"),
 	}
 	return utils.Validate(r, tps, nil)
 }
