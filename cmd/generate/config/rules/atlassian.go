e3607e6bbbad99cfbe0d106be2b1a9f153f3c6fa
Oscar Blanco Castan <oscarbc1996@gmail.com>
2024-10-28 16:49:36 +0100

    apply comments from PR

--- cmd/generate/config/rules/atlassian.go
+++ cmd/generate/config/rules/atlassian.go
@@ -14,10 +14,10 @@ func Atlassian() *config.Rule {
 		Regex: utils.MergeRegexps(
 			utils.GenerateSemiGenericRegex(
 				[]string{"atlassian", "confluence", "jira"},
-				`[a-zA-Z0-9{24}`,
+				`[a-zA-Z0-9]{24}`,
 				false,
 			),
-			utils.GenerateUniqueTokenRegex(`ATATT3[\w-]{177}=[A-F0-9]{8}`, false),
+			utils.GenerateUniqueTokenRegex(`ATATT3[A-Za-z0-9_\-=]{186}`, false),
 		),
 		Entropy:  3.5,
 		Keywords: []string{"atlassian", "confluence", "jira", "atatt3"},
