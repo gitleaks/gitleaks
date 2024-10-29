5fe58bf0b0be1735ad27aa6053b56323a905c223
Ben-grmbl <173737458+Ben-grmbl@users.noreply.github.com>
2024-10-29 22:33:09 +0100

    fix(rules): entropy, uppercase in samples (#1593)
    `go generate` was slightly flaky,
    since the example true positive secrets for these rules
    did not include uppercase letters
    and/or the min entropy was just barely too high

--- cmd/generate/config/rules/hashicorp_vault.go
+++ cmd/generate/config/rules/hashicorp_vault.go
@@ -28,10 +28,10 @@ func VaultServiceToken() *config.Rule {
 	// validate
 	tps := []string{
 		// Old
-		utils.GenerateSampleSecret("vault", "s."+secrets.NewSecret(utils.AlphaNumeric("24"))),
+		utils.GenerateSampleSecret("vault", secrets.NewSecret(`s\.[a-zA-Z0-9]{24}`)),
 		`token: s.ZC9Ecf4M5g9o34Q6RkzGsj0z`,
 		// New
-		utils.GenerateSampleSecret("vault", "hvs."+secrets.NewSecret(utils.AlphaNumericExtendedShort("90"))),
+		utils.GenerateSampleSecret("vault", secrets.NewSecret(`hvs\.[\w\-]{90}`)),
 		`-vaultToken hvs.CAESIP2jTxc9S2K7Z6CtcFWQv7-044m_oSsxnPE1H3nF89l3GiYKHGh2cy5sQmlIZVNyTWJNcDRsYWJpQjlhYjVlb1cQh6PL8wEYAg"`, // longer than 100 chars
 	}
 	fps := []string{
