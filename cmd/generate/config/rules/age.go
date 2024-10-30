437dec039d352028bf1f9562618bd0995c1c8609
Richard Gomez <rmgomez368@gmail.com>
2024-10-30 16:34:50 -0400

    fix merge issues

--- cmd/generate/config/rules/age.go
+++ cmd/generate/config/rules/age.go
@@ -2,6 +2,7 @@ package rules
 
 import (
 	"github.com/zricethezav/gitleaks/v8/cmd/generate/config/utils"
+	"github.com/zricethezav/gitleaks/v8/cmd/generate/secrets"
 	"github.com/zricethezav/gitleaks/v8/config"
 	"github.com/zricethezav/gitleaks/v8/regexp"
 )
@@ -12,15 +13,13 @@ func AgeSecretKey() *config.Rule {
 		RuleID:      "age-secret-key",
 		Description: "Discovered a potential Age encryption tool secret key, risking data decryption and unauthorized access to sensitive information.",
 		// TODO: This character range is confusing. Huh?
-		Regex:    regexp.MustCompile(`AGE-SECRET-KEY-1[QPZRY9X8GF2TVDW0S3JN54KHCE6MUA7L]{58}`),
+		Regex:    regexp.MustCompile(`\bAGE-SECRET-KEY-1[QPZRY9X8GF2TVDW0S3JN54KHCE6MUA7L]{58}\b`),
 		Entropy:  3,
 		Keywords: []string{"AGE-SECRET-KEY-1"},
 	}
 
 	// validate
-	tps := []string{
-		`apiKey := "AGE-SECRET-KEY-1FDFPAKWWTNRY2FTQ22EKVUM67LZ48RQQFDNH0V90D6SVDKCSCDXSNP5KFA`, // gitleaks:allow
-	}
+	tps := utils.GenerateSampleSecrets("age", secrets.NewSecret(`AGE-SECRET-KEY-1[QPZRY9X8GF2TVDW0S3JN54KHCE6MUA7L]{58}`)) // gitleaks:allow
 	fps := []string{
 		`apiKey := "AGE-SECRET-KEY-1QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ`, // gitleaks:allow
 	}
