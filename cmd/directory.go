915397acfe7b76ebf8935dd39052a4738d28a99e
Braxton Plaxco <bplaxco@redhat.com>
2025-05-18 03:49:03 -0400

    Keep cmd the same for now

--- cmd/directory.go
+++ cmd/directory.go
@@ -58,7 +58,7 @@ func runDirectory(cmd *cobra.Command, args []string) {
 		logging.Fatal().Err(err).Msg("could not get exit code")
 	}
 
-	var paths <-chan sources.Source
+	var paths <-chan sources.ScanTarget
 	paths, err = sources.DirectoryTargets(
 		source,
 		detector.Sema,
