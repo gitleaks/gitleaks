6f251eef65b76d0078d0583672385d4657ad7a87
Kashif Khan <70996046+kashifkhan0771@users.noreply.github.com>
2025-06-05 05:47:19 +0500

    Added Heroku API Key New Version (#1883)
    * Added Heroku API Key New Version
    
    * updated rule

--- cmd/generate/config/rules/heroku.go
+++ cmd/generate/config/rules/heroku.go
@@ -24,3 +24,22 @@ func Heroku() *config.Rule {
 	)
 	return utils.Validate(r, tps, nil)
 }
+
+func HerokuV2() *config.Rule {
+	// define rule
+	r := config.Rule{
+		Description: "Detected a Heroku API Key, potentially compromising cloud application deployments and operational security.",
+		RuleID:      "heroku-api-key-v2",
+		Regex:       utils.GenerateUniqueTokenRegex(`(HRKU-AA[0-9a-zA-Z_-]{58})`, false),
+		Entropy:     4,
+		Keywords:    []string{"HRKU-AA"},
+	}
+
+	// validate
+	tps := utils.GenerateSampleSecrets("heroku", secrets.NewSecret(`\b(HRKU-AA[0-9a-zA-Z_-]{58})\b`))
+	tps = append(tps,
+		`const KEY = "HRKU-AAlQ1aVoHDujJ9QsDHdHlHO0hbzhoERRSO45ZQusSYHg_____w4_hLrAym_u""`,
+		`API_Key = "HRKU-AAy9Ppr_HD2pPuTyIiTYInO0hbzhoERRSO93ZQusSYHgaD7_WQ07FnF7L9FX"`,
+	)
+	return utils.Validate(r, tps, nil)
+}
