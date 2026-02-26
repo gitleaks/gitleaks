b8afa05439ecc2fb223c87c4b7566fa3eaa823ce
Elio Neto <netoo.elio@hotmail.com>
2026-02-26 19:14:04 -0300

    fix: remove unused secrets import
    - Remove import of secrets package (not used with static test cases)
    - Fixes build error: imported and not used
    - Keep only utils and config imports

--- cmd/generate/config/rules/sourcegraph.go
+++ cmd/generate/config/rules/sourcegraph.go
@@ -2,7 +2,6 @@ package rules
 
 import (
 	"github.com/zricethezav/gitleaks/v8/cmd/generate/config/utils"
-	"github.com/zricethezav/gitleaks/v8/cmd/generate/secrets"
 	"github.com/zricethezav/gitleaks/v8/config"
 )
 
